3712791ebd4e92ac2d6d1db48defc8fa2cb2fb3c
gopirajumaruboina <gopirajumaruboina1@gmail.com>
2022-03-01 17:00:07 +0530

    Adding Employee proj

---
+++ Prjgit/Prjgit/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prjgit
+{
+    
+    internal class Employee
+    {
+        public int id { get; set; }
+        public string name { get; set; } 
+        
+        public void getemployee()
+        {
+
+        }
+        public void displayemploye()
+        {
+
+        }
+    }
+}
